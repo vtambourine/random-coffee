c130d04a20f2700811fec3832c30508ca18dde4b
Stefan Poschina <stefan.poschina@booking.com>
2018-06-28 13:58:19 +0200

    Fixed formatting

--- db.go
+++ db.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "strconv"
-    _ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"strconv"
 )
 
 func db_test() {
-    database, _ := sql.Open("sqlite3", "./storage.db")
-    statement, _ := database.Prepare("INSERT INTO employee (email_address) VALUES (?)")
-    statement.Exec("test@booking.com")
-    rows, _ := database.Query("SELECT * FROM employee")
-    fmt.Println("%v", rows)
-    var id int
-    var email string
-    var active int
-    for rows.Next() {
-        rows.Scan(&id, &email, &active)
-        fmt.Println(strconv.Itoa(id) + " : " + email + " : " + strconv.Itoa(active))
-    }
+	database, _ := sql.Open("sqlite3", "./storage.db")
+	statement, _ := database.Prepare("INSERT INTO employee (email_address) VALUES (?)")
+	statement.Exec("test@booking.com")
+	rows, _ := database.Query("SELECT * FROM employee")
+	fmt.Println("%v", rows)
+	var id int
+	var email string
+	var active int
+	for rows.Next() {
+		rows.Scan(&id, &email, &active)
+		fmt.Println(strconv.Itoa(id) + " : " + email + " : " + strconv.Itoa(active))
+	}
 }
